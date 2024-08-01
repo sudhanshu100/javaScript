51590bc458055d09351e1680d89fd68f01aac339
Sudhanshu sharma <139452518+sudhanshu100@users.noreply.github.com>
2024-08-01 23:46:09 +0530

    Add files via upload

---
+++ mail.word_count.js
@@ -0,0 +1,3 @@
+var mail = prompt("enter your mail:");
+var total_letters = mail.length;
+alert("your mail has" + total_letters + "letters and you have" + 140 - total_letters + " letters remaining");
