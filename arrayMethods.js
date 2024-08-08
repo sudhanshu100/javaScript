77d61c5a9f64a4e440c15539cbc161962bf3ddfa
Sudhanshu sharma <139452518+sudhanshu100@users.noreply.github.com>
2024-08-08 21:29:00 +0530

    Add files via upload

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,14 +1,28 @@
-let companies = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
-console.log(companies);
+//filter out the marks above 90 using filter method...
 
-//Remove the first company from array-
-companies.shift();
-console.log(companies);
+let marks = [ 87, 93, 64, 99, 86 ];
+let toppers = marks.filter((val) => {
+    return val > 90;
 
-//Add Amazon at the end-
-companies.push("Amazon");
-console.log(companies);
+})
+console.log(toppers);
 
-//Remove Uber and add Ola in it's place-
-companies.splice(2,1,"Ola");
-console.log(companies);
+// take a no. n as input from user, creat an array from 1 to n. use reduce method to :-
+// i) calculate sum of all numbers in array
+// ii) calculate product of all numbers in array
+
+let num = prompt("enter a number");
+let arr=[];
+for( let i=1; i<=num; i++){
+    arr[i-1]=i;
+}
+console.log(arr);
+let sum = arr.reduce((prev, curr) => {
+    return prev+curr;
+})
+console.log("sum of array elements:-" +sum);
+let prod = arr.reduce((prev, curr) => {
+    return prev*curr;
+})
+console.log("product of array elements:-" + prod);
+
