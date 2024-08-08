84672362859a76e267774db274ef18507ad4f6b7
Sudhanshu sharma <139452518+sudhanshu100@users.noreply.github.com>
2024-08-08 16:12:26 +0530

    Add files via upload

---
+++ forEachLoop.js
@@ -0,0 +1,14 @@
+//for a given array of numbers print the square of each value using the forEach loop...
+
+const nums = [2,3,4,5,6];
+//either
+nums.forEach((val) => {
+   console.log(val*val);
+})
+
+//or
+
+//  let calcSquare = (num) => {
+//     console.log(num*num);
+// };
+// nums.forEach(calcSquare);
